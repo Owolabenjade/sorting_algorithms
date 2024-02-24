2339b4bc9e05a9c37677cfaad3fd1d585a31be45
Benjamin <owolabenjade@gmail.com>
2024-02-24 10:22:56 -0800

    power3

--- sort.h
+++ sort.h
@@ -2,6 +2,7 @@
 #define SORT_H
 
 #include <stddef.h>
+#include <stdio.h>
 
 /* Provided structure for doubly linked list */
 typedef struct listint_s
@@ -17,5 +18,10 @@ void print_list(const listint_t *list);
 
 /* Function prototype for selection_sort */
 void selection_sort(int *array, size_t size);
+
+void quick_sort(int *array, size_t size);
+void quick_sort_recursive(int *array, int low, int high, size_t size);
+int lomuto_partition(int *array, int low, int high, size_t size);
+void swap(int *a, int *b);
 
 #endif /* SORT_H */
