f20e87e375b79881887718b06c4dca90df12106b
Benjamin <owolabenjade@gmail.com>
2024-02-24 15:05:37 -0800

    omo

--- sort.h
+++ sort.h
@@ -1,14 +1,16 @@
 #ifndef SORT_H
 #define SORT_H
 
-#include <stddef.h>
+#include <stdlib.h>
 
-// Function prototypes for sorting functions
-void bubble_sort(int *array, size_t size);
-void insertion_sort_list(listint_t **list);
+typedef struct listint_s
+{
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
+} listint_t;
 
-// Prototype for printing functions (if applicable)
 void print_array(const int *array, size_t size);
-void print_list(const listint_t *list);
+void bubble_sort(int *array, size_t size);
 
-#endif
+#endif /* SORT_H */
