50b93a40df50e790c0cd4ed1d87a9c24ced9556c
Benjamin <owolabenjade@gmail.com>
2024-02-24 10:32:38 -0800

    God

--- sort.h
+++ sort.h
@@ -2,7 +2,6 @@
 #define SORT_H
 
 #include <stddef.h>
-#include <stdio.h>
 
 /* Provided structure for doubly linked list */
 typedef struct listint_s
@@ -23,5 +22,6 @@ void quick_sort(int *array, size_t size);
 void quick_sort_recursive(int *array, int low, int high, size_t size);
 int lomuto_partition(int *array, int low, int high, size_t size);
 void swap(int *a, int *b);
+void print_list(const listint_t *list);
 
 #endif /* SORT_H */
