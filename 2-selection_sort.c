1d279968f0cc7148e7b9b0e93bac6c1028c7207a
Benjamin <owolabenjade@gmail.com>
2024-02-24 10:16:05 -0800

    hello

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,25 +1,20 @@
 #include "sort.h"
 
 /**
- * selection_sort - Sorts an array of integers in ascending order using
- * the Selection sort algorithm.
- *
+ * selection_sort - Sorts an array of integers
+ * using the Selection sort algorithm
  * @array: The array to be sorted
- * @size: Number of elements in the array
+ * @size: Number of elements in @array
  */
 void selection_sort(int *array, size_t size)
 {
-	if (array == NULL || size < 2)
-		return;
-
 	size_t i, j, min_idx;
-	int temp;
 
-	for (i = 0; i < size - 1; i++)
+	for (i = 0; i < size - 1; ++i)
 	{
 		min_idx = i;
 
-		for (j = i + 1; j < size; j++)
+		for (j = i + 1; j < size; ++j)
 		{
 			if (array[j] < array[min_idx])
 				min_idx = j;
@@ -27,10 +22,10 @@ void selection_sort(int *array, size_t size)
 
 		if (min_idx != i)
 		{
-			temp = array[i];
+			/* Swap elements and print array */
+			int temp = array[i];
 			array[i] = array[min_idx];
 			array[min_idx] = temp;
-
 			print_array(array, size);
 		}
 	}
--- sort.h
+++ sort.h
@@ -1,51 +1,21 @@
 #ifndef SORT_H
 #define SORT_H
 
-#include <stdlib.h>
+#include <stddef.h>
 
-/**
- * struct listint_s - Doubly linked list node
- *
- * @n: Integer stored in the node
- * @prev: Pointer to the previous element of the list
- * @next: Pointer to the next element of the list
- */
+/* Provided structure for doubly linked list */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
-/**
- * print_array - Prints an array of integers
- *
- * @array: The array to be printed
- * @size: Number of elements in @array
- */
+/* Provided functions for printing array and list */
 void print_array(const int *array, size_t size);
-
-/**
- * print_list - Prints a list of integers
- *
- * @list: The list to be printed
- */
 void print_list(const listint_t *list);
-void bubble_sort(int *array, size_t size);
-/**
- * insertion_sort_list - Sorts a doubly linked list of integers in ascending order
- * using the Insertion sort algorithm.
- *
- * @list: Pointer to the head of the doubly linked list
- */
-void insertion_sort_list(listint_t **list);
 
-/**
- * selection_sort - Sorts an array of integers in ascending order using
- * the Selection sort algorithm.
- *
- * @array: The array to be sorted
- * @size: Number of elements in the array
- */
+/* Function prototype for selection_sort */
 void selection_sort(int *array, size_t size);
+
 #endif /* SORT_H */
