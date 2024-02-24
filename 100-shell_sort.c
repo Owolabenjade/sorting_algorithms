8a0b76443ba516f3cb45d73b02ab192aa149665b
Benjamin <owolabenjade@gmail.com>
2024-02-24 15:42:51 -0800

    Chike

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,40 +1,40 @@
 #include "sort.h"
 
 /**
- * shell_sort - Sorts an array of integers
- * in ascending order using Shell sort
- * algorithm with the Knuth sequence
+ * shell_sort - Sorts an array of integers in
+ * ascending order using the Shell sort
+ * algorithm (Knuth sequence)
  * @array: The array to be sorted
- * @size: Number of elements in @array
+ * @size: The size of the array
  */
 void shell_sort(int *array, size_t size)
 {
 	size_t gap = 1, i, j;
 	int temp;
 
-	if (!array || size < 2)
-		return;
-
+	/*Calculate initial gap using Knuth's sequence*/
 	while (gap < size / 3)
+	{
 		gap = gap * 3 + 1;
+	}
 
+	/*Start with the largest gap and work down to a gap of 1*/
 	while (gap > 0)
 	{
 		for (i = gap; i < size; i++)
 		{
 			temp = array[i];
-			j = i;
-
-			while (j >= gap && array[j - gap] > temp)
+			for (j = i; j >= gap && array[j - gap] > temp; j -= gap)
 			{
 				array[j] = array[j - gap];
-				j -= gap;
 			}
-
 			array[j] = temp;
 		}
 
-		gap = (gap - 1) / 3;
+		/*Print the array after each interval decrease*/
 		print_array(array, size);
+
+		/*Decrease gap for next stage*/
+		gap = (gap - 1) / 3;
 	}
 }
