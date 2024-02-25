31305e92a9f5cb70f5a3e1596d16ec8e673b6c3c
Benjamin <owolabenjade@gmail.com>
2024-02-25 01:51:28 -0800

    woto

--- 106-bitonic_sort.c
+++ 106-bitonic_sort.c
@@ -92,23 +92,3 @@ void bitonic_sort(int *array, size_t size)
 
 	bitonic_sort_rec(array, size, 0, size, 1);
 }
-
-/**
- * print_array - Prints an array of integers.
- * @array: The array to be printed.
- * @size: Number of elements in @array.
- */
-void print_array(const int *array, size_t size)
-{
-	size_t i;
-
-	i = 0;
-	while (array && i < size)
-	{
-		if (i > 0)
-			printf(", ");
-		printf("%d", array[i]);
-		++i;
-	}
-	printf("\n");
-}
